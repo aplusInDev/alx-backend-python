fdd66967ec797e7ed2441c4b6e317dd5c93dde8b
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-01-11 20:58:31 +0100

    updating task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Module that takes a list and returns a list  """
-from typing import List
+from typing import List, Tuple
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Function that takes a list lst of integers and floats elements and
     returns their sum as a float"""
     zoomed_in: List = [
@@ -13,7 +13,7 @@ def zoom_array(lst: List, factor: int = 2) -> List:
     return zoomed_in
 
 
-array: List = [12, 72, 91]
+array: Tuple = (12, 72, 91)
 
 zoom_2x: List = zoom_array(array)
 
