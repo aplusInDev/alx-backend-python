852ca05cc6bdf83d691dd25eee99a0c17d43d504
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-01-11 20:55:48 +0100

    updating task 11

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,6 +9,8 @@ Deflt = Union[T, None]
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Deflt = None) -> Return:
+    """Given the parameters and the return values,
+    add type annotations to the function"""
     if key in dct:
         return dct[key]
     else:
